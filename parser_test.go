35409c870076340129e415c7539dd59c8c30bba1
Emanuel Valente <emanuel.valente@ifood.com.br>
2023-09-19 16:30:05 -0300

    improve tests on parser

--- parser_test.go
+++ parser_test.go
@@ -29,6 +29,12 @@ func TestMatchCPF(t *testing.T) {
 		expect bool
 	}{
 		{"11144477735", true},
+		{"111444777-35", true},
+		{"111.44477735", true},
+		{"111.444.77735", true},
+		{"111.444.777-35", true},
+		{`111.444.777-35"`, true},
+		{`111.444.777-35"]}`, true},
 		{"11144477734", false},
 		{"", false},
 	}
@@ -48,12 +54,67 @@ func TestMatchCNPJ(t *testing.T) {
 	}{
 		{"11.444.777/0001-61", true},
 		{"14.380.200/0001-21", true},
+		{"14.380.2000001-21", true},
+		{"143802000001-21", true},
+		{"14.380.2000001-21", true},
+		{"14380200/000121", true},
+		{`14380200/000121"`, true},
+		{`14380200/000121"]}`, true},
+		{`14380200/000122"`, false},
 		{"11.444.777/0001-60", false},
 		{"", false},
 	}
 
 	for _, test := range tests {
 		got := matchCNPJ(test.input)
+		if got != test.expect {
+			t.Errorf("For input %q expected %v but got %v", test.input, test.expect, got)
+		}
+	}
+}
+
+func TestMatchIP(t *testing.T) {
+	tests := []struct {
+		input  string
+		expect bool
+	}{
+		{"192.168.0.1", true},
+		{"10.0.1.9", true},
+		{"109.200.3.90", true},
+		//TODO {"109.200.3.260", false},
+		{"2001:0db8:85a3:0000:0000:8a2e:0370:7334", true},
+		{"2001:0db8:85a3::8a2e:0370:7334", true},
+		{"2001:::::0370:7334", true},
+		{`2001:::::0370:7334"`, true},
+		{`2001:::::0370:7334"]}`, true},
+		{"", false},
+	}
+
+	for _, test := range tests {
+		got := matchip(test.input)
+		if got != test.expect {
+			t.Errorf("For input %q expected %v but got %v", test.input, test.expect, got)
+		}
+	}
+}
+
+func TestMatchEmail(t *testing.T) {
+	tests := []struct {
+		input  string
+		expect bool
+	}{
+		{"joaosilva@gmail.com", true},
+		{"joaosilva@mail.ru", true},
+		{"joaosilva@ifood.com.br", true},
+		{"joao.silva@ifood.com.br", true},
+		{"joao.silva@@ifood.com.br", false},
+		{`joao.silva@ifood.com.br"`, true},
+		{`joao.silva@ifood.com.br"]}`, true},
+		{"", false},
+	}
+
+	for _, test := range tests {
+		got := matchemail(test.input)
 		if got != test.expect {
 			t.Errorf("For input %q expected %v but got %v", test.input, test.expect, got)
 		}
